dde130c5a23827a484b124c4dd623e042753c2f1
hendraws <saputra.hendrawijaya@gmail.com>
2021-08-28 18:15:11 +0700

    hapus yang ga perlu

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 // dibawah ini dibutuhkan akses autitentifikasi
 Route::group(['middleware' => 'auth'], function () { 
 	Route::get('/home', 'HomeController@index')->name('home');
-	// Route::get('/report/{id}/delete', 'ReportController@delete');
-	// Route::resource('/report', 'ReportController');
 	Route::put('/program-kerja/{id}/reset', 'ProgramKerjaController@reset');
 	Route::get('/program-kerja/{id}/resetModal', 'ProgramKerjaController@resetModal');
 	Route::get('/program-kerja/{id}/delete', 'ProgramKerjaController@delete');
@@ -54,77 +52,7 @@ Route::group(['middleware' => 'auth'], function () {
 			return 'Clear Cache';
 		});
 
-		Route::get('/generate/program-kerja/{jml}', function($jml){
-		// Laravel Carbon subtract days from current date
-		// $date = Carbon::now()->subDays(0);
-			$jumlah = $jml;
-			for ($i=0; $i < $jumlah ; $i++) { 
-				$bulan = Carbon::now()->subMonth($i);
-				$cabang = KantorCabang::get();
-				foreach ($cabang as $value) {
-					$cabang = $value->id;
-					$drop = rand(10000000,25000000);
-					$storting = $drop * 5;
-					$psp = $drop*0.05;
-					$drop_tunda = rand(10000,750000);
-					$storting_tunda = $drop_tunda * 0.1;
-					$tkp = $storting - ($drop / 100 * 91) - $psp;
-					$sisa_kas = rand(1000000,25000000);
-					ProgramKerja::Create(
-						[
-							"cabang" => $cabang,
-							"tanggal" => $bulan,
-							"drops" => $drop,
-							"storting" => $storting,
-							"psp" => $psp,
-							"drop_tunda" => $drop_tunda,
-							"storting_tunda" => $storting_tunda,
-							"tkp" => $tkp,
-							"sisa_kas" => $sisa_kas,
-							'created_by' => auth()->user()->id,
-							'updated_by' => auth()->user()->id,
-						]
-					);
-				}
-			}
-			return 'berhasil generate';
-		});
-
-		Route::get('/generate/perkembangan/{jml}', function($jml){
-		// Laravel Carbon subtract days from current date
-		// $date = Carbon::now()->subDays(0);
-			$jumlah = $jml;
-			for ($i=0; $i < $jumlah ; $i++) { 
-				$tanggal = Carbon::now()->subDays($i);
-				$cabang = KantorCabang::get();
-				foreach ($cabang as $value) {
-					$cabang = $value->id;
-					$drop = rand(1000000,2500000);
-					$storting = $drop * 1.2;
-					$psp = $drop*0.05;
-					$drop_tunda = rand(1000,75000);
-					$storting_tunda = $drop_tunda * 0.1;
-					$tkp = $storting - ($drop / 100 * 91) - $psp;
-					$sisa_kas = rand(100000,2500000);
-					Perkembangan::Create(
-						[
-							"cabang" => $cabang,
-							"tanggal" => $tanggal,
-							"drops" => $drop,
-							"storting" => $storting,
-							"psp" => $psp,
-							"drop_tunda" => $drop_tunda,
-							"storting_tunda" => $storting_tunda,
-							"tkp" => $tkp,
-							"sisa_kas" => $sisa_kas,
-							'created_by' => auth()->user()->id,
-							'updated_by' => auth()->user()->id,
-						]
-					);
-				}
-			}
-			return 'berhasil generate';
-		});
+		
 	});
 	
 });
